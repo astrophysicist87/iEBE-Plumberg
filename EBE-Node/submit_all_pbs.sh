b07b1f75ff321f1cfdfe3efc6344b4c2ac767e0e
Christopher Plumberg <astrophysicist87@gmail.com>
2017-11-27 17:06:17 -0500

    A few more cosmetic changes.

--- EBE-Node/submit_all_pbs.sh
+++ EBE-Node/submit_all_pbs.sh
@@ -20,49 +20,59 @@ nMaxProcessesRunning=12
 #submit jobs
 for ((i=111; i<=1000; i++))
 do
+	##########################################
+	# before submitting any more jobs, make sure you aren't at the max. limit
+	##########################################
+	echo 'Waiting to submit results-'$i'...'
+	nProcessesRunning=`qstat -u plumberg.1 | grep plumberg.1 | awk '$(NF-1)=="R"' | wc -l`
+	until [ "$nProcessesRunning" -lt "$nMaxProcessesRunning" ]
+	do
+		#echo $nProcessesRunning '==' $nMaxProcessesRunning "processes currently running at" `date` >> $outfile
+		sleep 10
 		nProcessesRunning=`qstat -u plumberg.1 | grep plumberg.1 | awk '$(NF-1)=="R"' | wc -l`
-		until [ "$nProcessesRunning" -lt "$nMaxProcessesRunning" ]
-		do
-			#echo $nProcessesRunning '==' $nMaxProcessesRunning "processes currently running at" `date` >> $outfile
-			sleep 10
-			nProcessesRunning=`qstat -u plumberg.1 | grep plumberg.1 | awk '$(NF-1)=="R"' | wc -l`
-		done
-		echo $nProcessesRunning '<' $nMaxProcessesRunning "processes currently running at" `date` >> $outfile
+	done
+	echo $nProcessesRunning '<' $nMaxProcessesRunning "processes currently running at" `date` >> $outfile
+	echo 'Submitting results-'$i'.'
+	##########################################
 
-		npt0=15
-		npphi0=36
-		npy0=15
-		nqt0=17
-		nqx0=7
-		nqy0=7
-		nqz0=7
-		resfrac=0.60
+	npt0=15
+	npphi0=36
+	npy0=15
+	nqt0=17
+	nqx0=7
+	nqy0=7
+	nqz0=7
+	resfrac=0.60
 
-		workingDirectory='/home/plumberg.1/Plumberg_iEBE/iEBE-stable/all_hydro_results/results-'`echo $i`
+	workingDirectory='/home/plumberg.1/Plumberg_iEBE/iEBE-stable/all_hydro_results/results-'`echo $i`
 
-		lwd=$homeDirectory/results-`echo $i`
-		mkdir $lwd
-		mkdir $lwd/results
+	lwd=$homeDirectory/results-`echo $i`
+	mkdir $lwd
+	mkdir $lwd/results
 
-		cp $workingDirectory/decdat2.dat $workingDirectory/decdat_mu.dat $workingDirectory/surface.dat $lwd/results
+	cp $workingDirectory/decdat2.dat $workingDirectory/decdat_mu.dat $workingDirectory/surface.dat $lwd/results
 
-		newPBSscriptName=run_cfwr.pbs
-		cp run_cfwr.pbs $lwd/$newPBSscriptName
-		(
-			cd $lwd
+	##########################################
+	##########################################
+	newPBSscriptName=run_cfwr.pbs
+	cp run_cfwr.pbs $lwd/$newPBSscriptName
+	(
+		cd $lwd
 
-			\cp $srcDirec/cfwr.e .
-			\cp $srcDirec/parameters.dat .
-			\cp -r $srcDirec/EOS .
+		\cp $srcDirec/cfwr.e .
+		\cp $srcDirec/parameters.dat .
+		\cp -r $srcDirec/EOS .
 
-			echo 'Results directory and submission ID:' $i \
-					`qsub -v workingDirectory=$lwd,NPT=$npt0,NPPHI=$npphi0,NPY=$npy0,NQT=$nqt0,NQX=$nqx0,NQY=$nqy0,NQZ=$nqz0,RESFRAC=$resfrac $newPBSscriptName` >> $outfile
-			cd ..;
-			echo 'Submitted' $i 'at' `date` >> $outfile
-			sleep 3
-			#update list of currently running jobs in case I need to kill all currently running jobs
-			qstat -u plumberg.1 | grep plumberg.1 | awk '$(NF-1)=="R"' | awk -F. '{print $1}' > $jobIDsfile
-		)
+		echo 'Results directory and submission ID:' $i \
+				`qsub -v workingDirectory=$lwd,NPT=$npt0,NPPHI=$npphi0,NPY=$npy0,NQT=$nqt0,NQX=$nqx0,NQY=$nqy0,NQZ=$nqz0,RESFRAC=$resfrac $newPBSscriptName` >> $outfile
+		cd ..;
+		echo 'Submitted' $i 'at' `date` >> $outfile
+		sleep 3
+		#update list of currently running jobs in case I need to kill all currently running jobs
+		qstat -u plumberg.1 | grep plumberg.1 | awk '$(NF-1)=="R"' | awk -F. '{print $1}' > $jobIDsfile
+	)
+	##########################################
+	##########################################
 done
 
 # End of file
