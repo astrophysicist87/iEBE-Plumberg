459b08ee7d916f16b910a364b148db240ab4c776
Christopher Plumberg <astrophysicist87@gmail.com>
2018-11-09 05:37:13 -0500

    Updated version of ParameterDict.py

--- ParameterDict.py
+++ ParameterDict.py
@@ -7,10 +7,10 @@ controlParameterList = {
 }
 
 initial_condition_control = {
-    'centrality': '0-1%',  # centrality bin
+    'centrality': '0-10%',  # centrality bin
     'cut_type': 'total_entropy',
     # centrality cut variable: total_entropy or Npart
-    'initial_condition_type': 'pre-generated',
+    'initial_condition_type': 'superMC',
     # type of initial conditions: superMC or pre-generated
     'pre-generated_initial_file_path': 'smooth_ICs', 
     # file path for the pre-generated initial condition files
@@ -27,7 +27,7 @@ superMCParameters = {
     'finalFactor'                   :   28.656,
     'alpha'                         :   0.14,      # WN/BC mixing ratio in MCGlb
     'lambda'                        :   0.288,      # saturation scale parameter in MCKLN
-    'operation'                     :   3,
+    'operation'                     :   1,
     'include_NN_correlation'        :   1,
     'cc_fluctuation_model'          :   6,
     'cc_fluctuation_Gamma_theta'    :   0.75,       
@@ -62,10 +62,10 @@ hydroParameters = {
                               # need to be the same as dx in superMC
     'dy'        :   0.10,     # lattice spacing in y (fm)
                               # need to be the same as dy in superMC
-    'Edec'      :   0.5,  #
-    'ndx'       :   1,
-    'ndy'       :   1,
-    'ndt'       :   1,
+    'Edec'      :   0.3,  #
+    'ndx'       :   2,
+    'ndy'       :   2,
+    'ndt'       :   5,
     'IhydroJetoutput' :   0,  # switch for output hydro evolution history
     'InitialURead'    :   0,  # set it to be 1 when simulation_type == hydroEM_preEquilibrium
     'Initialpitensor' :   1,  # initialization of pi tensor
@@ -107,7 +107,7 @@ photonEmissionParameters = {
 }
 
 HoTCoffeehParameters = {
-	'include_delta_f'					:	1,
+    'include_delta_f'                   :   1,
     'SV_resonanceThreshold'             :   0.60,
     'CF_resonanceThreshold'             :   0.60,
     'use_log_fit'                       :   1,
