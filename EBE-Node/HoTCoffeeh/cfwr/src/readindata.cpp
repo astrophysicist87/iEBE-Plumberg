b7d259f635162b330a24690abee4465ec5dcbde4
Christopher Plumberg <plumberg.1@osu.edu>
2020-11-24 17:44:15 -0600

    Update readindata.cpp

--- EBE-Node/HoTCoffeeh/cfwr/src/readindata.cpp
+++ EBE-Node/HoTCoffeeh/cfwr/src/readindata.cpp
@@ -601,7 +601,8 @@ void get_important_resonances(int chosen_target_particle_idx, vector<int> * chos
 	vector<double> percent_contributions;
 	for (int i = 0; i < Nparticle; i++)
 		percent_contributions.push_back(particle[i].percent_contribution);
-	vector<size_t> sorted_resonance_indices = ordered(percent_contributions);
+	//vector<size_t> sorted_resonance_indices = ordered(percent_contributions);
+	vector<size_t> sorted_resonance_indices(10);	//WRONG
 	reverse(sorted_resonance_indices.begin(), sorted_resonance_indices.end());
 	//vector<int> chosen_resonance_indices_ptr;
 	//double running_total_percentage = 0.0;
