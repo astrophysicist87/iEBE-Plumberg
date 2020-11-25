41a5a74106a45a8b77c208840d539d4b3e4f8fd4
Christopher Plumberg <plumberg.1@osu.edu>
2020-11-24 21:08:12 -0600

    Update VISH2p1V1_moreOutput.10.0.for

--- EBE-Node/VISHNew/VISH2p1V1_moreOutput.10.0.for
+++ EBE-Node/VISHNew/VISH2p1V1_moreOutput.10.0.for
@@ -4794,6 +4794,7 @@ C            Print *, 'time',time,'Stotal', Stotal,StotalSv,StotalBv
 
         !Print everything to file
         !NOTA BENE: cf. Subroutine checkPi(...) for normalization of pi33!!!
+      if(Ed(I,J,NZ0)*Hbarc .ge. Edec) then
         write(2297, '(26e15.5)')Time, I*DX, J*DY, 
      &                  Temp(I,J,NZ0)*Hbarc, 
      &                  Ed(I,J,NZ0)*Hbarc, 
@@ -4818,6 +4819,7 @@ C            Print *, 'time',time,'Stotal', Stotal,StotalSv,StotalBv
      &                  phi7Grid(I,J,NZ0), 
      &                  taupipiGrid(I,J,NZ0), 
      &                  piSigmaGrid(I,J,NZ0)*Hbarc
+      endif
 
       enddo
       enddo
