fbfac243926083c4a230d52613950d6085356b48
Christopher Plumberg <plumberg.1@osu.edu>
2020-12-02 16:25:03 -0600

    Update updateParameterDict.py

--- updateParameterDict.py
+++ updateParameterDict.py
@@ -1,14 +1,31 @@
 import ParameterDict
 import sys
 
+def boolify(s):
+    if s == 'True':
+        return True
+    if s == 'False':
+        return False
+    raise ValueError("huh?")
+
+def autoconvert(s):
+    for fn in (boolify, int, float):
+        try:
+            return fn(s)
+        except ValueError:
+            pass
+    return s
+
 dictionaries = [item for item in dir(ParameterDict) if not item.startswith("__")]
 
 # Loop over command-line arguments
 for parameterChange in sys.argv[2:]:
     # parse this argument
     [name, parameter, value] = ((parameterChange.replace(":"," ")).replace("="," ")).split()
-    if value=="False" or value=="false": value=False
-    elif value=="True" or value=="true": value=True
+    #if value=="False" or value=="false": value=False
+    #elif value=="True" or value=="true": value=True
+    
+    value = autoconvert(value)
     
     # add this dictionary if not already included
     if not name in dictionaries:
@@ -20,7 +37,7 @@ for parameterChange in sys.argv[2:]:
         dictionary[parameter] = value
 
 # Print out updated ParameterDict
-outFile = sys.argv[1]
+outFile = sys.argv[1] #.replace(".","_").replace("%","").replace("-","_")
 with open(outFile, 'w') as f:
     dictionaries = [item for item in dir(ParameterDict) if not item.startswith("__")]
     for name in dictionaries:
